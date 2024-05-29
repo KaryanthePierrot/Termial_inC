636653679d22d35c8fbf9333c4ba749a97cfac79
Wes (Karyan) <113315411+KaryanthePierrot@users.noreply.github.com>
2024-05-29 13:27:09 -0300

    Add files via upload

---
+++ atividade 6 -- Termial/atividade 6.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <stdio.h>
+
+int main(void){
+	int i, s;
+	printf("numero? ");
+	scanf("%d", &i);
+	for(int c=i; c>=0;c--)
+		s = c+s;
+	printf("%d\n", s);
+	return 0;
+	}
